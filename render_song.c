22e9e248900f7dc9a1ec835d3b54405ef5390d1b
Abhi Mohnani <amohnan1@jhu.edu>
2019-10-11 16:38:35 -0400

    edited render_song.c and redid parse file method

--- render_song.c
+++ render_song.c
@@ -4,28 +4,41 @@
 #include "io.h"
 #include "wave.h"
 
-int parse_file(FILE *, char[], double**);
-void free_mem(char[] , double**, int);
+//int parse_file(FILE *, char[], double**);
+//void free_mem(char[] , double**, int);
+void parse_file(FILE *);
 
 int main(int argc, char *argv[]){
-  if (argc != 2){
+  if (argc != 3){
     fatal_error("Incorrect number of files in command line arguments.");
   }
-  FILE *input = fopen(argv[0], "r");
+  FILE *input = fopen(argv[1], "r");
   int num_samples;
   fscanf(input, " %d", &num_samples);
   int samples_per_beat;
   fscanf(input, " %d", &samples_per_beat);
+  printf(" %d %d", num_samples, samples_per_beat);
   
-  char * directives;
+  /*char * directives;
   double **nums;
   int num_directives = parse_file(input, directives, nums);
+  */
 
-  free_mem(directives, nums, num_directives);
+  
+  //free_mem(directives, nums, num_directives);
   fclose(input);
 }
 
-int parse_file(FILE *fptr, char[] directives, double** nums){
+void parse_file(FILE *input){
+  char directive;
+  int v;
+  while (fscanf(input, " %c", directive) != EOF){
+    
+  }
+}
+
+/*
+int parse_file(FILE *fptr, char* directives, double** nums){
   int count = 0;
   char direct;
   double num;
@@ -35,14 +48,14 @@ int parse_file(FILE *fptr, char[] directives, double** nums){
   for (int j = count; j < 10; j++){
     nums[j] = malloc(4*sizeof(double));
   }
-  while (fscanf(fptr, " %c", &direct) == 1){
+  while (fscanf(fptr, " %c", &direct) != EOF){
     if (count % 10 == 0 && count != 0){
-      directives = realloc(count, sizeof(char));
-      nums = realloc(count, sizeof(double*));
+      directives = realloc(directives, (count+10)*sizeof(char));
+      nums = realloc(nums,(count+10)* sizeof(double*));
       for (int j = count; j < count +  10; j++){
 	nums[j] = malloc(4*sizeof(double));
       }
-    }
+      }
     directives[count] = direct;
     while (fscanf(fptr, " %lf", &num) == 1 && i < 4){
       nums[count][i] = num;
@@ -50,14 +63,16 @@ int parse_file(FILE *fptr, char[] directives, double** nums){
     }
     i = 0;
     count++;
-  }
+    }
   return count;
 }
 
-void free_mem(char[] directives, double** nums, int count){
+void free_mem(char* directives, double** nums, int count){
   for (int i = 0; i < count; i++){
-    nums[i];
+    free(nums[i]);
+    
   }
   free(nums);
   free(directives);
-}
+  }
+*/
