f4eb9a7884147eb0c54c8cb060da0ebbd1f99bbb
Abhi Mohnani <amohnan1@jhu.edu>
2019-10-15 15:35:12 -0400

    render_song.c works for all directives besides chords

--- render_song.c
+++ render_song.c
@@ -6,36 +6,59 @@
 
 //int parse_file(FILE *, char[], double**);
 //void free_mem(char[] , double**, int);
-void parse_file(FILE *);
 
 int main(int argc, char *argv[]){
   if (argc != 3){
     fatal_error("Incorrect number of files in command line arguments.");
   }
-  FILE *input = fopen(argv[1], "r");
+  FILE *input = fopen(argv[1], "rb");
+  FILE *output = fopen(argv[2], "wb");
   int num_samples;
   fscanf(input, " %d", &num_samples);
   int samples_per_beat;
   fscanf(input, " %d", &samples_per_beat);
   printf(" %d %d", num_samples, samples_per_beat);
-  
+  int16_t buf[num_samples];
   /*char * directives;
   double **nums;
   int num_directives = parse_file(input, directives, nums);
   */
-
+  char directive;
+  unsigned voice = 0;
+  float amplitude = 0.1;
+  float length, freq_hz;
+  int note = 0;
+  int current = 0;
+  while (fscanf(input, " %c", &directive) == 1){
+    if (directive == 'N'){
+      fscanf(input, " %f %d", &length, &note);
+      freq_hz = 440 * pow(2, (note-69)/12);
+      render_voice_stereo((buf + current), length*samples_per_beat, freq_hz, amplitude, voice);
+      current += length*samples_per_beat;
+    }else if (directive == 'P'){
+      fscanf(input, " %f", &length);
+      current += length*samples_per_beat;
+    }else if (directive == 'C'){
+      
+    }else if (directive == 'V'){
+      fscanf(input, " %u", &voice);
+      if (voice >2){
+	fatal_error("Malformed input\n");
+      }
+    }else if (directive == 'A'){
+      fscanf(input, " %f", &amplitude);
+      if (amplitude < 0 || amplitude > 1){
+	fatal_error("Malformed input\n");
+      }
+    }
+  }
   
+  write_wave_header(output, (unsigned)num_samples);
+  write_s16_buf(output, buf, (unsigned)num_samples);
   //free_mem(directives, nums, num_directives);
   fclose(input);
 }
 
-void parse_file(FILE *input){
-  char directive;
-  int v;
-  while (fscanf(input, " %c", directive) != EOF){
-    
-  }
-}
 
 /*
 int parse_file(FILE *fptr, char* directives, double** nums){
